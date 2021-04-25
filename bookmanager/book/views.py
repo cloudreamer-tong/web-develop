cf54ba317a54fd8192a12af5a7e9ed4db73fd867
liutong <cloudream0802@outlook.com>
2021-04-26 00:22:07 +0800

    路由匹配2

--- bookmanager/book/views.py
+++ bookmanager/book/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+# 必须返回一个响应
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse('ok')
--- bookmanager/bookmanager/urls.py
+++ bookmanager/bookmanager/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import  include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/',include('book.urls'))  # 第一个参数可以理解为子应用
 ]
